3f5ffa06b0de95e9667785f1cc42817c98f53072
1afzal <afzalali5256@gmail.com>
2025-12-30 21:23:37 +0530

    Map

---
+++ Map/src/index.ts
@@ -0,0 +1,43 @@
+async function fecthAndMap() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos'); // array
+    const data = await response.json();
+
+    const title: string[] = data.map((d: any) => d.title);
+    const isCompleted: boolean[] = data.map((d: any)=> d.completed)
+    console.log(title, isCompleted);
+
+}
+// fecthAndMap()
+
+//map methods: 
+// set, get, size, delete, has
+interface student{
+    name: string,
+    age: number
+}
+
+const students = new Map<string, student>();
+
+students.set("john", { name: "john", age: 22 });
+students.set("tina", { name: "tina", age: 21 });
+students.set("Doe", { name: "Doe", age: 23 });
+
+const john = students.get("john");
+console.log(john);
+
+const size = students.size
+console.log(size)
+
+// students.delete("john");
+// students.clear(); //clears or deletes all 
+
+students.has("tina"); //returns a boolean value (true or false)
+
+students.forEach((i)=>{
+    console.log(i);
+})
+
+
+const arr: number[] = [10,20,30,40];
+const square = arr.map((m)=>m*2)
+console.log(square);
