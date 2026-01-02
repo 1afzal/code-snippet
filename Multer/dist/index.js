4d34c670d4434489dfe49d2a1aa379fb6d495e50
1afzal <afzalali5256@gmail.com>
2026-01-02 12:20:10 +0530

    Multer

---
+++ Multer/dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const multer_js_1 = __importDefault(require("./middleware/multer.js"));
+const app = (0, express_1.default)();
+app.post("/upload", multer_js_1.default.single("file"), (req, res) => {
+    res.status(200).json({
+        message: "upload succesfull"
+    });
+});
+app.listen(6969, () => {
+    console.log('server live at port 6969');
+});
+//# sourceMappingURL=index.js.map
---
+++ Multer/src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import upload from "./middleware/multer.js";
+const app = express()
+
+app.post("/upload",upload.single("file"), (req,res)=>{
+    res.status(200).json({
+        message: "upload succesfull"
+    });
+});
+
+app.listen(6969, ()=>{
+    console.log('server live at port 6969');
+});
