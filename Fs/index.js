183a2394e017a22c3fb508bf560c706a5679ff50
1afzal <afzalali5256@gmail.com>
2026-01-06 22:30:00 +0530

    FS-Module

---
+++ Fs/index.js
@@ -0,0 +1,60 @@
+import fs from "fs/promises";
+import fss from "fs";
+
+// Async read (Promise-based)
+async function readAsync() {
+  try {
+    const data = await fs.readFile("./info.txt", "utf-8");
+    console.log(data);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+// Sync read
+function readDataSync(){
+    const syncData = fss.readFileSync("info.txt", "utf-8");
+    console.log(syncData);
+}
+
+// Async write
+async function asyncWriteFs() {
+  try {
+    await fs.writeFile("demo.txt", "This is a demo message", "utf-8");
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function WriteFilesSync(){
+    fss.writeFileSync('demo.txt',"This is 2nd message", 'utf-8');
+    console.log("Written Syncronously succesfully")
+}
+
+function AppendFile(){
+    const log = `${new Date().toISOString()}`
+    try{
+        fs.appendFile('demo.log',log,'utf-8');
+        console.log("Appended succesfully");
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+async function WriteMultiple(){
+    try{
+        const file = await fs.open(`demo.txt`, 'w');
+        await file.write("line-1\n", "utf-8");
+        await file.write("line-2", "utf-8");
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+// readAsync();
+// asyncWriteFs();
+// readDataSync();
+//WriteFilesSync()
+// AppendFile();
+// WriteMultiple()
