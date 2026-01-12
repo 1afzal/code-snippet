2246cd7316746928f0d3162d30c809479b533b36
1afzal <afzalali5256@gmail.com>
2026-01-12 16:03:50 +0530

    ladder

---
+++ setInterval/ladder.js
@@ -0,0 +1,15 @@
+setTimeout(()=>{
+    console.log("eat breakfast")
+        setTimeout(()=>{
+            console.log("eat lunch")
+                setTimeout(()=>{
+                    console.log("eat supper")
+                        setTimeout(()=>{
+                            console.log("eat dinner")
+                                setTimeout(()=>{
+                                    console.log("sleep")
+                                },1000)
+                        },1000)
+                },1000)
+        },1000)
+},1000)
