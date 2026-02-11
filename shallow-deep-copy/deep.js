40e19d6f44cd2573e4534df0c37ec9cb076e351e
afza1 <afzalali5256@gmail.com>
2026-02-11 12:50:01 +0530

    deep-shallow-examples

--- shallow-deep-copy/deep.js
+++ shallow-deep-copy/deep.js
@@ -11,11 +11,35 @@ const user = {
 
 const deepcopy = JSON.parse(JSON.stringify(user));
 const structuredDeepcopy = structuredClone(user)
-
 deepcopy.name = "test name"
 console.log(deepcopy.name)
 console.log(deepcopy.address.pincode.secondDigit)
 deepcopy.address.pincode.secondDigit = 6969696
 console.log(deepcopy.address.pincode.secondDigit)
 
-//copies and everything can be altered
+// Nested elements can be altered
+
+const student = {
+    name: "Pinky Sharma",
+    usn: "4SF22IS007",
+    address: {
+        city: "Mangalore",
+        pincode: 574199
+    }
+}
+const deepyCopy = JSON.parse(JSON.stringify(student))
+const deepCopy = structuredClone(student); //this method is preferred
+
+
+const newName = deepCopy.name = "deep-name";
+const oldName = student.name;
+const newCity = deepCopy.address.city = "deep-user-city";
+const oldCity  = student.address.city;
+
+
+console.log(`New name -> ${newName}`);
+console.log(`old name -> ${student.name}`);
+console.log(`new city  -> ${newCity}`);
+console.log(`old city ->$ ${oldCity}`);
+
+
