0f9168a1e7385c97971eaede9cb8d4e21bb87297
afza1 <afzalali5256@gmail.com>
2026-02-11 12:42:47 +0530

    shallow copy examples

--- shallow-deep-copy/shallow.js
+++ shallow-deep-copy/shallow.js
@@ -1,14 +1,38 @@
+// const user = {
+//     name: "John Doe",
+//     address: {
+//         city: "Mangalore"
+//     }
+// }
+// const newUser = { ...user };
+// newUser.name = "Afzal Ali Ahmed "
+// newUser.address.city = "changed city "
+// console.log(user.name);
+// console.log(newUser.address.city );
+
+// //Top-level property name was copied.
+// //Nested object address was not copied — only its reference was copied.
+
+
+
+
+
+
+
 const user = {
-    name: "John Doe",
-    address: {
-        city: "Mangalore"
+    name: 'Afzal Ali Ahmed',
+    address : {
+        city: "mangalore"
     }
 }
-const newUser = { ...user };
-newUser.name = "Afzal Ali Ahmed "
-newUser.address.city = "changed city "
-console.log(user.name);
-console.log(newUser.address.city );
+const shallowUser = { ...user };
+const newName = shallowUser.name = "shallow-name";
+const oldName = user.name;
+const newCity = shallowUser.address.city = "shallow-user-city";
+const oldCity  = user.address.city;
 
-//Top-level property name was copied.
-//Nested object address was not copied — only its reference was copied.
+console.log(`New name -> ${newName}`);
+console.log(`old name -> ${user.name}`);
+console.log(`new city  -> ${newCity}`);
+console.log(`old city ->$ ${oldCity}`);
+
